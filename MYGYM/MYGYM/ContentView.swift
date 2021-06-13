40f5d16a7ddad3fadbe7bcff0700952a78c5e2ec
mohammed <mdrame113@gmail.com>
2021-06-13 16:51:03 -0400

    Project initilized/ Started

---
+++ MYGYM/MYGYM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MYGYM
+//
+//  Created by Mohammed Drame on 6/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MYGYM/MYGYM/MYGYMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYGYMApp.swift
+//  MYGYM
+//
+//  Created by Mohammed Drame on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct MYGYMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
